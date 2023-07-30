cb979bdacdd083da9aa66183e77a7d1f74b922fc
Eric Junokas <95892758+Ericofff@users.noreply.github.com>
2023-07-30 11:48:39 -0300

    variaveis

---
+++ js/home.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px', 
+    duration: 2500,
+    delay: 400
+  });
+
+ 
+  ScrollReveal().reveal('.content .h1', { delay: 500, origin: 'left' });
+  ScrollReveal().reveal('.content .success', { delay: 500, origin: 'right' });
+  ScrollReveal().reveal('.content p', { delay: 1000, origin: 'left' });
