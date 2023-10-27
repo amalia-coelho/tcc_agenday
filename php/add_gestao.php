eecb69d6d5c6a021a6faf9eb2743a8df87d8daef
Amália Coelho <gray.coelho@gmail.com>
2023-10-26 23:13:13 -0300

    gestao crud (semi pronto)
    add membro - OK
    delete membro - OK
    alterar membro - 1/2 OK

--- php/add_gestao.php
+++ php/add_gestao.php
@@ -1,22 +1,45 @@
 <?php
-	$cadastrar_gestao = "";
-	include('conexao.php');
-	try {
-	  $stmt = $conn->prepare('INSERT INTO tb_gestao (nm_membro, ds_cargo) VALUES(:nome, :cargo)');
-	  $stmt->execute(array(
-	    ':nome' => $_POST['nm_membro'],
-	    ':cargo' => $_POST['ds_cargo'],
-	  ));
+    try {
+        session_start();
+        include('conexao.php');
 
-	  //SELECT
-	  $sql = 'SELECT * FROM tb_gestao';
-	  foreach ($conn->query($sql) as $row) {
-	  	$cadastrar_gestao .=$row['nm_membro']."   ";
-	  	$cadastrar_gestao .=$row['ds_cargo']."   ";
-	  }
-	  echo $cadastrar_gestao;
+        function obterCaminhoSubpasta() {
+            $dirAtual = __DIR__;
+            $subpasta = $dirAtual . '/../uploads/gestao';
+            return $subpasta;
+        }
 
-	} catch(PDOException $e) {
- 		echo 'Error: ' . $e->getMessage();
-	}
+        // Verificar se um arquivo foi enviado
+        if (isset($_FILES['ds_imagem'])) {
+            $arquivo = $_FILES['ds_imagem'];
+            $subpasta = obterCaminhoSubpasta();
+
+            // Verifica se a pasta já existe, se não, a cria
+            if (!is_dir($subpasta)) {
+                mkdir($subpasta, 0777, true);
+            }
+
+            $caminhoDestino = $subpasta . '/' . $arquivo['name'];
+
+            if (move_uploaded_file($arquivo['tmp_name'], $caminhoDestino)) {
+                $stmt = $conn->prepare('INSERT INTO tb_gestao (nm_membro, ds_cargo, ds_imagem, dt_modificacao, id_nivel) VALUES(:nome, :cargo, :imagem, NOW(), :nivel)');
+                $stmt->execute(array(
+                    ':nome' => $_POST['nm_membro'],
+                    ':cargo' => $_POST['ds_cargo'],
+                    ':imagem' => "uploads/gestao/".$arquivo['name'],
+                    ':nivel' => 3
+                ));
+                
+                echo '<div class="alert" style="background-color: #DCEED7; border-left: 8px solid #9EB0A0; animation: none;"><i class="bi bi-check-lg "style="color: #9EB0A0";"></i><span class="msg" style="color: #9EB0A0; font-size:18px;">Membro adicionado!</span></div>';
+                echo "<meta http-equiv='refresh' content='1'>";
+            } else {
+                echo '<div class="alert"><i class="bi bi-exclamation-circle"></i><span class="msg" style="font-size: 17px">Erro no envio da imagem!.</span></div>';        
+            }
+        } else {
+            echo '<div class="alert"><i class="bi bi-exclamation-circle"></i><span class="msg" style="font-size: 17px">Erro! Tente novamente!</span></div>';
+        }   
+    } catch (PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+        echo "<br>" . $stmt->rowCount();
+    }
 ?>
