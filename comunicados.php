fd871ddb067dbba7888f092ca7667d5b678d48bd
Eric Junokas <95892758+Ericofff@users.noreply.github.com>
2023-11-10 11:48:28 -0300

    Search bar comunicado

--- comunicados.php
+++ comunicados.php
@@ -222,8 +222,31 @@
                   </ul>
                 </div>
         </div>
-      </div>
-    </div>
+	</div>
+</div>
+<!-- search bar-->
+<div class="centro-search">
+
+	<div class="inner-form">
+		<div class="input-field">
+<button class="btn-search" type="button">
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+	<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+</svg>
+</button>
+<input id="search" type="text" placeholder="Pesquise aqui"/>
+</div>
+</div>
+<div class="suggestion-wrap">
+	<span>Etec</span>
+	<span>Evento</span>
+	<span>Escolar</span>
+	<span>Aviso</span>
+	<span style="background:red" id="clearSearch">Limpar <i class="bi bi-x-circle-fill"></i></span>
+</div>
+</div>
+
+<!-- fim da search bar-->
 		<?php
 			$sql = "SELECT * FROM tb_comunicado";
 
@@ -462,6 +485,60 @@
 		selecttexto.innerHTML = "Mais Antigo";
    }
 	</script>
+				<SCRIpt>
+				  $(document).ready(function(){
+    // Evento de clique para spans dentro de suggestion-wrap
+    $(".suggestion-wrap span").click(function(){
+      // Atualiza o valor do campo de pesquisa com o texto do span clicado
+      $("#search").val($(this).text());
+
+      // Executa a função de filtro
+      filterCards();
+    });
+
+    // Função de filtro
+    function filterCards() {
+      var searchTerm = $("#search").val().toLowerCase();
+
+      $(".comunicado").each(function(){
+        var cardTitle = $(this).find(".comunicado-header").text().toLowerCase();
+        var cardSub = $(this).find(".comunicado-text").text().toLowerCase();
+
+        if(cardTitle.includes(searchTerm) || cardSub.includes(searchTerm)){
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    }
+    // Evento de clique para o span "Limpar"
+    $("#clearSearch").click(function(){
+      // Limpa o valor do campo de pesquisa
+      $("#search").val('');
+
+      // Executa a função de filtro
+      filterCards();
+    });
+    // Evento de clique para o botão de pesquisa
+    $(".btn-search").click(function(){
+      filterCards();
+    });
+
+    // Evento de tecla para pesquisa em tempo real
+    $("#search").on('keyup', function(){
+      filterCards();
+    });
+  });
+				const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+const expand = () => {
+  searchBtn.classList.toggle("close");
+  input.classList.toggle("square");
+};
+
+searchBtn.addEventListener("click", expand);
+			</SCRIpt>
 </body>
 
 </html>
