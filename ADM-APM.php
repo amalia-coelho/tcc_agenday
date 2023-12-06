1e68494923d067d5b27218056ed0012b450c38a5
Eric Junokas <95892758+Ericofff@users.noreply.github.com>
2023-12-05 23:01:24 -0300

    Barra de pesquisa

--- ADM-APM.php
+++ ADM-APM.php
@@ -169,6 +169,27 @@ if (!isset($_SESSION['email'])) {
 					<p>Olá, aqui é a APM (Associação de Pais e Mestres)! Nessa área é possível somente visualizar todos os itens que são vendidos na nossa escola, caso você queira adquirir alguma coisa, terá que se redirecionar para a secretária da Etec de Itanhaém.</p>
 				</div>
 
+				
+			<!-- search bar-->
+
+			<div class="inner-form">
+            <div class="input-field">
+              <button class="btn-search" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                </svg>
+              </button>
+              <input id="search" type="text" placeholder="Pesquise aqui"/>
+            </div>
+          </div>
+		  <div class="suggestion-wrap">
+		  <span>Camiseta</span>
+            <span>Informática</span>
+            <span>Administração</span>
+            <span>Armários</span>
+            <span style="background:red" id="clearSearch">Limpar <i class="bi bi-x-circle-fill"></i></span>
+          </div>
+
 			<!-- fim da search bar-->
 
 				<div class="apm-card-container">
