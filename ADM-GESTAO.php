78ce12e24650fe790e3435b37cf82e8c42c0eec5
Eric Junokas <95892758+Ericofff@users.noreply.github.com>
2023-12-05 23:20:02 -0300

    Filtro gestão

--- ADM-GESTAO.php
+++ ADM-GESTAO.php
@@ -253,7 +253,7 @@ if (!isset($_SESSION['email'])) {
     <i class="bi bi-plus-circle-fill"></i> <!-- Ícone de adição -->
   </button>
 
-  <!-- Modal de ADICIONAR APM -->
+  <!-- Modal de ADICIONAR GESTAO -->
   <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
     aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -302,7 +302,54 @@ if (!isset($_SESSION['email'])) {
     </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
+<script>
+	
+$(document).ready(function(){
+    // Evento de clique para spans dentro de suggestion-wrap
+    $(".suggestion-wrap span").click(function(){
+        // Atualiza o valor do campo de pesquisa com o texto do span clicado
+        $("#search").val($(this).text());
 
+        // Executa a função de filtro
+        filterCards();
+    });
+
+    // Função de filtro
+    function filterCards() {
+        var searchTerm = $("#search").val().toLowerCase();
+
+        $(".gestao-users").each(function(){
+            var memberName = $(this).find("p:first").text().toLowerCase();
+            var memberPosition = $(this).find("p:last").text().toLowerCase();
+
+            if(memberName.includes(searchTerm) || memberPosition.includes(searchTerm)){
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    }
+
+    // Evento de clique para o span "Limpar"
+    $("#clearSearch").click(function(){
+        // Limpa o valor do campo de pesquisa
+        $("#search").val('');
+
+        // Executa a função de filtro
+        filterCards();
+    });
+
+    // Evento de clique para o botão de pesquisa
+    $(".btn-search").click(function(){
+        filterCards();
+    });
+
+    // Evento de tecla para pesquisa em tempo real
+    $("#search").on('keyup', function(){
+        filterCards();
+    });
+});
+</script>
 </html>
 <?php
 	}
